972f73ed335e58d573319857933dc66fcbc2ec3e
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-10-26 22:43:25 +1100

    Create 20240411123456_create_announcements.rbncements.rb

---
+++ db/migrate/20240411123456_create_announcements.rbncements.rb
@@ -0,0 +1,8 @@
+class CreateAnnouncements < ActiveRecord::Migration[6.1]
+  def change
+    create_table :announcements do |t|
+      t.text :content, null: false
+
+      t.timestamps
+    end
+  end
